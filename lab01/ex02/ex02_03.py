640638c595f0abd1fbcd7a6e474ce4ae51671fd6
DQuang <quangkd040404@gmail.com>
2025-04-02 13:28:55 +0700

    Delete lab01 directory

--- lab01/ex02/ex02_03.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Nhập một số nguyên: "))
-
-if num % 2 == 0:
-    print (num, "là số chẵn")
-else :
-    print (num, "là số lẻ")
--- lab01/hello.py
+++
@@ -1,3 +0,0 @@
-print ("Hello World!")
-print ("My name is Quang")
-print ("HUTECH University")
